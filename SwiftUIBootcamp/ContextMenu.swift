30a94cd14563c07e847762c7cca63d2f6aed099b
enessancar <enes57751907@gmail.com>
2023-10-26 22:01:35 +0300

    I created context menu using

---
+++ SwiftUIBootcamp/ContextMenu.swift
@@ -0,0 +1,18 @@
+//
+//  ContextMenu.swift
+//  SwiftUIBootcamp
+//
+//  Created by Enes Sancar on 26.10.2023.
+//
+
+import SwiftUI
+
+struct ContextMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContextMenu()
+}
